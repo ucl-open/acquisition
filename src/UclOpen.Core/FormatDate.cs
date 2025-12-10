137591b45bd14f9f35658d91c4ee739de3560604
NeuroThom <t.ryan@neurogears.org>
2025-12-10 17:00:02 +0000

    Add actual .cs operators

--- src/UclOpen.Core/FormatDate.cs
+++ src/UclOpen.Core/FormatDate.cs
@@ -1,18 +1,23 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
 
-namespace UclOpen.Core
+namespace Aeon.Acquisition
 {
     [Combinator]
     [Description("Formats a date time into a string with no illegal path characters.")]
     [WorkflowElementCategory(ElementCategory.Transform)]
     public class FormatDate
     {
-        public IObservable<string> Process(IObservable<DateTime> source);
-        public override bool Equals(object obj);
-        public override int GetHashCode();
-        public Type GetType();
-        public override string ToString();
+        public IObservable<string> Process(IObservable<DateTime> source)
+        {
+            return source.Select(value =>
+            {
+                var result = value.ToString("o").Replace(':', '-');
+                return result.Substring(0, result.IndexOf('.'));
+            });
+        }
     }
 }
--- src/UclOpen.Core/StripSubstring.cs
+++ src/UclOpen.Core/StripSubstring.cs
@@ -1,8 +1,10 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
 
-namespace UclOpen.Core
+namespace Aeon.Acquisition
 {
     [Combinator]
     [Description("Selects the substring preceding the first occurrence of the specified separator.")]
@@ -10,11 +12,14 @@ namespace UclOpen.Core
     public class StripSubstring
     {
         public string Separator { get; set; }
-        
-        public IObservable<string> Process(IObservable<string> source);
-        public override bool Equals(object obj);
-        public override int GetHashCode();
-        public Type GetType();
-        public override string ToString();
+
+        public IObservable<string> Process(IObservable<string> source)
+        {
+            return source.Select(value =>
+            {
+                var parts = value.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+                return parts.Length > 0 ? parts[0] : value;
+            });
+        }
     }
 }
