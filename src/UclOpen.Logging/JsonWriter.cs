1db65aed18db026b7b5c01c3bf115c03c3350735
RoboDoig <erskine94@gmail.com>
2025-12-11 16:02:36 +0000

    Add json writer to logging

---
+++ src/UclOpen.Logging/JsonWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bonsai.IO;
+
+namespace UclOpen.Logging
+{
+    public class JsonWriter : StreamSink<string, StreamWriter>
+    {
+        protected override StreamWriter CreateWriter(Stream stream)
+        {
+            return new StreamWriter(stream);
+        }
+
+        protected override void Write(StreamWriter writer, string input)
+        {
+            writer.WriteLine(input);
+        }
+    }
+}
